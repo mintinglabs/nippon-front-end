cf635ada7c27c6b4b2c8534db6aa89c905c4d896
hippye99 <hipp.ye@presslogic.com>
2025-12-09 11:00:36 +0800

    fix: CVE-2025-66478 漏洞 版本升级修复

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning={true} data-lt-installed="true">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
